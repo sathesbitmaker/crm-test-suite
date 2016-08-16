c5716e4b1b707669f9b537ae6cdb57f1cbc4bab9
NatalieBlack <natalievictoriablack@gmail.com>
2016-08-16 00:06:37 -0400

    make tests more readable

--- test/contact_test.rb
+++ test/contact_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative '../contact'
 
 class TestContact < Minitest::Test
@@ -12,34 +13,58 @@ class TestContact < Minitest::Test
   end
 
   def test_all
-    assert_equal [@contact], Contact.all
+    expected_value = [@contact]
+    actual_value = Contact.all
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_find
-    assert_equal @contact, Contact.find(@contact.id)
+    expected_value = @contact
+    actual_value = Contact.find(@contact.id)
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_find_by
-    assert_equal @contact, Contact.find_by('first_name', 'Grace')
+    expected_value = @contact
+    actual_value = Contact.find_by('first_name', 'Grace')
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_delete_all
     Contact.delete_all
-    assert_equal 0, Contact.all.size
+
+    expected_value = []
+    actual_value = Contact.all
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_full_name
-    assert_equal 'Grace Hopper', @contact.full_name
+    expected_value = 'Grace Hopper'
+    actual_value = @contact.full_name
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_update
     @contact.update('note', 'wrote the first compiler in 1952')
-    assert_equal 'wrote the first compiler in 1952', @contact.note
+
+    expected_value = 'wrote the first compiler in 1952'
+    actual_value = @contact.note
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_delete
     @contact.delete
-    assert [], Contact.all
+
+    expected_value = []
+    actual_value = Contact.all
+
+    assert_equal(expected_value, actual_value)
   end
 
 end
